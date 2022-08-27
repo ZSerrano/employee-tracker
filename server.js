052ea6650a71c4627ca2474ba17fc34466276b50
Brometheus <zs.serrano1@gmail.com>
2022-08-26 22:13:34 -0500

    added viewing for all tables

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const inquirer = require("inquirer");
 const mysql = require("mysql2");
+const cTable = require("console.table");
 
 const PORT = process.env.PORT || 3001;
 
@@ -12,3 +13,54 @@ const db = mysql.createConnection(
   },
   console.log(`Connected to the tracker_db database.`)
 );
+
+const viewDepartment = `SELECT * FROM department`;
+const searchDepartments = () => {
+  db.query(viewDepartment, (err, result) => {
+    if (err) {
+      console.log(err);
+    }
+    console.table(result);
+  });
+};
+
+const viewEmployee = `SELECT * FROM employee`;
+const searchEmployees = () => {
+  db.query(viewEmployee, (err, result) => {
+    if (err) {
+      console.log(err);
+    }
+    console.table(result);
+  });
+};
+
+const viewRole = `SELECT * FROM role`;
+const searchRoles = () => {
+  db.query(viewRole, (err, result) => {
+    if (err) {
+      console.log(err);
+    }
+    console.table(result);
+  });
+};
+
+const initView = () => {
+  return inquirer.prompt({
+    type: "list",
+    message: "What would you like to view?",
+    name: "choice",
+    choices: ["Departments", "Employees", "Roles", "End"],
+  });
+};
+
+initView().then(({ choice }) => {
+  if (choice === "Departments") {
+    return searchDepartments();
+  } else if (choice === "Employees") {
+    return searchEmployees();
+  } else if (choice === "Roles") {
+    return searchRoles();
+  } else {
+    console.log("Ended viewing.");
+  }
+});
